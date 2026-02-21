bfdfe85abf9e43a76b8d3a3da325deed84c441c2
MohsenParnadvar <mohsen.parandvar@ravanertebat.com>
2026-02-22 00:09:30 +0330

    changing the tag of image is implemented

--- internal/docker/compose.go
+++ internal/docker/compose.go
@@ -2,7 +2,9 @@ package docker
 
 import (
 	"os"
+	"strings"
 
+	"github.com/MohsenParandvar/reployer/internal/errs"
 	"gopkg.in/yaml.v3"
 )
 
@@ -36,9 +38,79 @@ func GetComposeServices(composeFilePath string) (map[string]string, error) {
 
 func ChangeServiceTag(composeFilePath string, serviceName string, tag string) error {
 	file, err := os.ReadFile(composeFilePath)
-
 	if err != nil {
 		return err
 	}
 
+	var node yaml.Node
+	if err := yaml.Unmarshal(file, &node); err != nil {
+		return err
+	}
+
+	if err := SetServiceImage(&node, serviceName, tag); err != nil {
+		return err
+	}
+
+	exportFile, err := os.Create(composeFilePath)
+	if err != nil {
+		return err
+	}
+
+	encoder := yaml.NewEncoder(exportFile)
+	encoder.SetIndent(2)
+	if err := encoder.Encode(&node); err != nil {
+		encoder.Close()
+		return err
+	}
+	return encoder.Close()
+}
+
+func SetServiceImage(node *yaml.Node, serviceName string, tag string) error {
+	if node == nil || len(node.Content) == 0 {
+		return errs.ErrEmptyYamlFile
+	}
+
+	root := node.Content[0]
+	services, err := FindMappingChild(root, "services")
+	if err != nil {
+		return err
+	}
+
+	if services.Kind != yaml.MappingNode {
+		return errs.ErrServiceMappingNode
+	}
+
+	service, err := FindMappingChild(services, serviceName)
+	if err != nil {
+		return err
+	}
+
+	image, err := FindMappingChild(service, "image")
+	if err != nil {
+		return err
+	}
+
+	splittedImage := strings.Split(image.Value, ":")
+
+	image.Kind = yaml.ScalarNode
+	image.Tag = "!!str"
+	image.Value = splittedImage[0] + ":" + tag
+
+	return nil
+}
+
+func FindMappingChild(node *yaml.Node, key string) (*yaml.Node, error) {
+	if node == nil || node.Kind != yaml.MappingNode {
+		return nil, errs.ErrServiceMappingNode
+	}
+
+	for i := 0; i < len(node.Content); i += 2 {
+		k := node.Content[i]
+		v := node.Content[i+1]
+		if k.Value == key {
+			return v, nil
+		}
+	}
+
+	return nil, errs.ErrServiceNotFound
 }
--- internal/errs/errs.go
+++ internal/errs/errs.go
@@ -3,5 +3,8 @@ package errs
 import "errors"
 
 var (
-	ErrDockerDeamon = errors.New("docker daemon is not running/installed")
+	ErrDockerDeamon       = errors.New("docker daemon is not running/installed")
+	ErrEmptyYamlFile      = errors.New("Yaml file is empty")
+	ErrServiceNotFound    = errors.New("Service not found")
+	ErrServiceMappingNode = errors.New("Service is not a mapping node")
 )
